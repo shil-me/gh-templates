6c2540fd12a5c1cd19e706d68746e345c377af6f
Palash A <palash.agarwal.96@gmail.com>
2023-06-15 19:27:26 -0400

    fix install script

--- scripts/install.sh
+++ scripts/install.sh
@@ -9,14 +9,16 @@ git pull origin master
 echo "---- Installing templates"
 mkdir ../.github
 cp -r -f ISSUE_TEMPLATE ../.github
+echo "created new folder .github/ISSUE_TEMPLATE"
+# cp -r -f PULL_REQUEST_TEMPLATE ../.github
+# echo "created new folder .github/PULL_REQUEST_TEMPLATE"
 
 echo "---- Committing changes"
-git add ../.github
+cd ..
+git add .github
 git commit -m "install submodule https://github.com/shil-me/gh-templates.git gh-templates"
 
 echo "---- Done"
 echo "Run \`git push\` to push the changes to remote and see the templates in action."
 echo "Remember, for the templates to work, they must be included on the default branch of the repository."
-
-cd ..
 echo "goodbye"
